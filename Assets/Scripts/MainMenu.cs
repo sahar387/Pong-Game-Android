adf78ab8b1b8e750c775424b1c9d3df997c464fb
sahar387 <sahareftekhari680@gmail.com>
2024-12-09 22:59:39 +0330

    UPDATE: Main Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
@@ -10,8 +9,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        print("play");
-        //SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadSceneAsync(1);
     }
 
 
